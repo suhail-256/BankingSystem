29d57bae397bbce775e672744e1713b9cc384bd4
suhail-256 <suhail.abdelaal@gmail.com>
2025-11-16 11:26:22 +0200

    Add transaction history tracking

--- src/Bank.sol
+++ src/Bank.sol
@@ -2,14 +2,26 @@
 
 pragma solidity ^0.8.30;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 contract Bank {
     // Define the Account struct
+
+    using Strings for uint256;
+    using Strings for address;
 
     struct Account {
         address owner;
         string name;
         uint256 balance;
         bool exists;
+        string[] txHistory;
+    }
+
+    enum TxType {
+        Deposit,
+        Withdraw,
+        Transfer
     }
 
     // Mapping to store accounts by owner address
@@ -40,7 +52,8 @@ contract Bank {
     function createAccount(string memory _name) public {
         require(!accounts[msg.sender].exists, "Account already exists");
 
-        accounts[msg.sender] = Account({owner: msg.sender, name: _name, balance: 0, exists: true});
+        accounts[msg.sender] =
+            Account({owner: msg.sender, name: _name, balance: 0, exists: true, txHistory: new string[](0)});
 
         emit AccountCreated(msg.sender, _name);
     }
@@ -53,6 +66,8 @@ contract Bank {
         require(msg.value > 0, "Must send ETH to deposit");
 
         accounts[to].balance += msg.value;
+
+        _addTransactionHistory(msg.sender, to, TxType.Deposit, msg.value);
 
         emit DepositMade(msg.sender, to, msg.value);
     }
@@ -67,12 +82,24 @@ contract Bank {
 
         // 2. Effects
         accounts[msg.sender].balance -= amount;
+        _addTransactionHistory(msg.sender, msg.sender, TxType.Withdraw, amount);
 
         // 3. Interactions
         (bool success,) = payable(msg.sender).call{value: amount}("");
         require(success, "Transfer failed");
 
         emit WithdrawalMade(msg.sender, amount);
+    }
+
+    // function to get transaction history => only account owner can preforme
+    function getTransactionHistory() public view onlyAccountOwner returns (string[] memory) {
+        require(accounts[msg.sender].exists, "Account does not exist");
+        return accounts[msg.sender].txHistory;
+    }
+
+    // function to clear the transaction history => only account owner can preforme
+    function clearHistory() public onlyAccountOwner {
+        accounts[msg.sender].txHistory = new string[](0);
     }
 
     // function to check the account balance => only account owner can preforme
@@ -89,8 +116,8 @@ contract Bank {
         require(accounts[msg.sender].balance >= amount, "Insufficient balance");
 
         accounts[msg.sender].balance -= amount;
-
         accounts[to].balance += amount;
+        _addTransactionHistory(msg.sender, to, TxType.Transfer, amount);
 
         emit TransferMade(msg.sender, to, amount);
     }
@@ -111,4 +138,43 @@ contract Bank {
         Account memory account = accounts[owner];
         return (account.owner, account.name, account.balance, account.exists);
     }
+
+    // Private methods
+
+    // add transaction history to the account
+    function _addTransactionHistory(address from, address to, TxType txType, uint256 amount) private {
+
+        // convert the timestamp, amount, from, and to to strings
+        string memory timestamp = block.timestamp.toString();
+        string memory amountStr = amount.toString();
+        string memory fromStr = from.toHexString();
+        string memory toStr = to.toHexString();
+        
+        if (txType == TxType.Deposit) {
+            if (to == msg.sender) {
+                accounts[to].txHistory
+                    .push(string(abi.encodePacked("Deposit: ", amountStr, " to self", " on: ", timestamp)));
+            } else {
+                accounts[to].txHistory
+                    .push(string(abi.encodePacked("Deposit: ", amountStr, " to ", toStr, " on: ", timestamp)));
+            }
+        } else if (txType == TxType.Withdraw) {
+            accounts[from].txHistory.push(string(abi.encodePacked("Withdraw: ", amountStr, " on: ", timestamp)));
+        } else if (txType == TxType.Transfer) {
+            accounts[from].txHistory
+                .push(string(abi.encodePacked("Transfer: ", amountStr, " to ", toStr, " on: ", timestamp)));
+            accounts[to].txHistory
+                .push(string(abi.encodePacked("Transfer: ", amountStr, " from ", fromStr, " on: ", timestamp)));
+        }
+    }
+
+
+    // check if the transaction history length is greater than 30 and if it is, remove the oldest transaction
+    function _checkTransactionHistoryLength(address accountOwner) private {
+        if (accounts[accountOwner].txHistory.length >= 30) {
+            accounts[accountOwner].txHistory.pop();
+        }
+    }
+
+
 }
